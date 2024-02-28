998e325354248f1563f18aff87b22493a0c01637
Emrick <emrickthibault02@gmail.com>
2024-02-28 15:01:17 -0500

    script

---
+++ Motor maker unity/Assets/MechanicalLibrary/Gearbox/Gear.cs
@@ -0,0 +1,32 @@
+﻿namespace Mechanix
+{
+    public class Gear : IMechanicalPiece
+    {
+        private int nbDents;
+        private double rayon;
+
+        public Gear()
+        {
+            nbDents = 0;
+            rayon = 0;
+        }
+
+        public Gear(int dents, double rayon)
+        {
+            this.nbDents = dents;
+            this.rayon = rayon;
+        }
+
+        public int NbDents
+        {
+            get => nbDents;
+            set => nbDents = value;
+        }
+
+        public double Rayon
+        {
+            get => rayon;
+            set => rayon = value;
+        }
+    }
+}
---
+++ Motor maker unity/Assets/MechanicalLibrary/MonoBehaviour1.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Mechanix
+{
+    public class MonoBehaviour1 : MonoBehaviour
+    {
+    }
+}
