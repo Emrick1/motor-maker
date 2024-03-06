ca69c8cf7639c2b60676ece93c8737db8f1c340c
Emrick <emrickthibault02@gmail.com>
2024-03-06 16:37:08 -0500

    engine qui change selon celui choisi

--- Motor maker unity/Assets/MechanicalLibrary/Mechanix/Engine.cs
+++ Motor maker unity/Assets/MechanicalLibrary/Mechanix/Engine.cs
@@ -3,22 +3,23 @@ using UnityEngine;
 
 namespace Mechanix
 {
-    public class Engine : MonoBehaviour, IMechanicalPiece
+    public class Engine : MonoBehaviour
     {
-      /*  private double mass; //masse du moteur (kg)
-        private double hp; //puissance du moteur (cheval vapeur)
-        private int rpmMax; //Rotations Par Minutes(RPM) Maximum
-        private int rpmMin; //Rotations Par Minutes(RPM) Minimum
-        private double torque; //rotation de base à la sortie du moteur(vers la transmission)
-        private double energyConsumption; //indice de consommation d'énergie(par seconde)
-      */
-        private string nameCylindre;
+        /*  private double mass; //masse du moteur (kg)
+          private double hp; //puissance du moteur (cheval vapeur)
+          private int rpmMax; //Rotations Par Minutes(RPM) Maximum
+          private int rpmMin; //Rotations Par Minutes(RPM) Minimum
+          private double torque; //rotation de base à la sortie du moteur(vers la transmission)
+          private double energyConsumption; //indice de consommation d'énergie(par seconde)
+        */
+        private string name;
+        
         
       
 
         public Engine(string name)
         {
-            this.nameCylindre = name;
+            this.name = name;
             /*
             this.mass = mass;
             this.hp = hp;
@@ -27,6 +28,11 @@ namespace Mechanix
             this.torque = torque;
             this.energyConsumption = energyConsumption;
             */
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
         }
         void start()
         {
--- Motor maker unity/Assets/MechanicalLibrary/Mechanix/engineList.cs
+++ Motor maker unity/Assets/MechanicalLibrary/Mechanix/engineList.cs
@@ -4,24 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using static Mechanix.Engine;
 
     public class engineList : MonoBehaviour
     {
 
         private static List<Engine> enginesList = new List<Engine>();
+        private static Engine currentEngine;
+        public TextMeshProUGUI engineSelected;
 
-        private static Engine engine1 = new Engine("v6");
+    private static Engine engine1 = new Engine("v6");
         private static Engine engine2 = new Engine("v8");
         private static Engine engine3 = new Engine("v10");
         private static Engine engine4 = new Engine("v12");
+        public Button buttonEngine1;
+        public Button buttonEngine2;
+        public Button buttonEngine3;
+        public Button buttonEngine4;
 
 
     private void Start()
     {
+        buttonEngine1.onClick.AddListener(delegate { SwitchEngineTo(0); });
+        buttonEngine2.onClick.AddListener(delegate { SwitchEngineTo(1); });
+        buttonEngine3.onClick.AddListener(delegate { SwitchEngineTo(2); });
+        buttonEngine4.onClick.AddListener(delegate { SwitchEngineTo(3); });
 
         addEngineToList();
+    }
+    public void SwitchEngineTo(int engineIndex)
+    {
+        currentEngine = enginesList[engineIndex];
+        engineSelected.text = enginesList[engineIndex].Name;
     }
 
 
