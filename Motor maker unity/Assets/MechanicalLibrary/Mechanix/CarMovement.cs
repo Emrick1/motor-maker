b37086e63181d54547d865043624e227c9968f2d
Andrei Iurii <2239249@cegeplimoilou.ca>
2024-03-20 17:29:31 -0400

    Update jeu et controle

--- Motor maker unity/Assets/MechanicalLibrary/Mechanix/CarMovement.cs
+++ Motor maker unity/Assets/MechanicalLibrary/Mechanix/CarMovement.cs
@@ -1,6 +1,9 @@
 using Mechanix;
+using System;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
@@ -12,21 +15,64 @@ namespace Mechanix
 
         [SerializeField] private Rigidbody _rb;
         [SerializeField] private Vector3 m_EulerAngleVelocity;
+        [SerializeField] private WheelCollider frontRight;
+        [SerializeField] private WheelCollider frontLeft;
+        [SerializeField] private WheelCollider rearRight;
+        [SerializeField] private WheelCollider rearLeft;
+        [SerializeField] private BoxCollider carCollider;
+        [SerializeField] private Transform frontRightTransform;
+        [SerializeField] private Transform frontLeftTransform;
+        [SerializeField] private Transform rearRightTransform;
+        [SerializeField] private Transform rearLeftTransform;
+        [SerializeField] private Vector3 rbVector = new Vector3(0, 0, 1);
         void Start()
         {
-            m_EulerAngleVelocity = new Vector3(0, 100, 0);
+            
         }
 
         void Update()
         {
-            var vel = new Vector3(0, 0, 1) * (float) PerfCalc.Speed;
+            Quaternion deltaRotationLeft = Quaternion.Euler(new Vector3(0, 20, 0) * Time.fixedDeltaTime);
+            Quaternion deltaRotationRight = Quaternion.Euler(new Vector3(0, -20, 0) * Time.fixedDeltaTime);
+
+            
+            var vel = rbVector * (float) PerfCalc.Speed;
             vel.y = _rb.velocity.y;
             _rb.velocity = vel;
+            int axis = 0;
             if (Input.GetKey(KeyCode.A))
             {
-                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
-                _rb.MoveRotation(_rb.rotation * deltaRotation);
+                _rb.MoveRotation(_rb.rotation * deltaRotationRight);
+                carCollider.transform.rotation = _rb.rotation * deltaRotationLeft;
+                axis = -1;
+                rbVector.y += 0.05f;
+            } else if (Input.GetKey(KeyCode.D))
+            {
+                _rb.MoveRotation(_rb.rotation * deltaRotationLeft);
+                carCollider.transform.rotation = _rb.rotation * deltaRotationRight;
+                axis = 1;
+                rbVector.y -= 0.05f;
             }
+
+            frontLeft.steerAngle = 15f * axis;
+            frontRight.steerAngle = 15f * axis;
+
+
+
+            UpdateWheel(frontLeft, frontLeftTransform);
+            UpdateWheel(frontRight, frontRightTransform);
+            UpdateWheel(rearLeft, rearLeftTransform);
+            UpdateWheel(rearRight, rearRightTransform);
+        }
+
+        private void UpdateWheel(WheelCollider col, Transform trans)
+        {
+            Vector3 position;
+            Quaternion rotation;
+            col.GetWorldPose(out position, out rotation);
+
+            trans.position = position;
+            trans.rotation = rotation;
         }
     }
 }
