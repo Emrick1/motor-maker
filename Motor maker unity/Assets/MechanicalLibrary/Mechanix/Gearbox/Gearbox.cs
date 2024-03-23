d3b9f8f8bc54f0de5fe8a503a1af111d52805dfa
Emrick <2238418@cegeplimoilou.ca>
2024-03-22 20:56:08 -0400

    gerbox animation fait par emrick et jean-gab

--- Motor maker unity/Assets/MechanicalLibrary/Mechanix/Gearbox/Gearbox.cs
+++ Motor maker unity/Assets/MechanicalLibrary/Mechanix/Gearbox/Gearbox.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Reflection;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,7 +14,7 @@ namespace Mechanix
         private static Gear currentGear;
         public TextMeshProUGUI gearSelected;
         public TextMeshProUGUI TextDents;
-        private static Gear gearMenante = new Gear(5, 1 , "Menante");
+        private static Gear gearMenante = new Gear(5, 1, "Menante");
         private static Gear gear1 = new Gear(10, 1, "Gear1");
         private static Gear gear2 = new Gear(14, 1, "Gear2");
         private static Gear gear3 = new Gear(18, 1, "Gear3");
@@ -27,6 +29,29 @@ namespace Mechanix
         public Button buttonGear4;
         public Button buttonGearLimitante;
         public Slider dentsSlider;
+        public GameObject gear10;
+        public GameObject gear11;
+        public GameObject gear12;
+        public GameObject gear13;
+        public GameObject gear14;
+        public GameObject gear15;
+        public GameObject gear16;
+        public GameObject gear17;
+        public GameObject gear18;
+        public GameObject gear19;
+        public GameObject gear20;
+        public GameObject gear21;
+        public GameObject gear22;
+        public GameObject gear23;
+        public GameObject gear24;
+        public GameObject gear25;
+        public GameObject gear26;
+        public GameObject gear27;
+        public GameObject gear28;
+        public GameObject gear29;
+        public GameObject gear30;
+        private static List<GameObject> affichageGears = new List<GameObject>();
+
 
         public Gearbox(List<Gear> gearsSet)
         {
@@ -42,10 +67,19 @@ namespace Mechanix
             buttonGear3.onClick.AddListener(delegate { SwitchGearTo(3); });
             buttonGear4.onClick.AddListener(delegate { SwitchGearTo(4); });
 
-                dentsSlider.onValueChanged.AddListener(delegate { setTextDents(); });
-            
-
+            dentsSlider.onValueChanged.AddListener(delegate { setTextDents(); setActiveGear(); });
+            initiationAffichageGears();
             addGearToList();
+        }
+
+        private void initiationAffichageGears()
+        {
+            for (int i = 10; i <= 30; i++)
+            {
+                string fieldName = "gear" + i;
+                FieldInfo field = GetType().GetField(fieldName);
+                affichageGears.Add((GameObject)field.GetValue(this));
+            }
         }
 
         public static void addGearToList()
@@ -58,6 +92,21 @@ namespace Mechanix
             gears.Add(gear5);
             gears.Add(gear6);
             gears.Add(gearLimitante);
+
+        }
+        public void setActiveGear()
+        {
+
+            foreach (GameObject g in affichageGears) {
+                if(int.Parse(g.name.Substring(4)) == currentGear.NbDents)
+                {
+                    g.SetActive(true);
+                } else
+                {
+                    g.SetActive(false);
+                }
+            }
+
 
         }
         public void SwitchGearTo(int gearIndex)
@@ -119,16 +168,17 @@ namespace Mechanix
                 dentsSlider.SetValueWithoutNotify(current.NbDents);
             
         }
-        void start()
-        {
-            
-        }
+        
 
         void Update()
         {
             if (currentGear != null)
             {
                 TextDents.text = currentGear.NbDents.ToString();
+                foreach (GameObject g in affichageGears)
+                {
+                    g.transform.Rotate(new Vector3(0.25f,1,0.5f), 100 * Time.deltaTime);
+                }
             }
         }
     }
